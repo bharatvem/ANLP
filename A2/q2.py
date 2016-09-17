fe1696744dcab9d04fe3697504b16ce5879e9441
Bharat Vemulapalli <bharatvemulapalli@gmail.com>
2016-09-17 16:19:39 -0400

    assignment #2 submitted

--- A2/q2.py
+++ A2/q2.py
@@ -1,19 +1,38 @@
-import nltk,math
+import nltk,math,numpy
 
+#Get the data
 t = "Bach was the most famous composer in the world, and so was Handel. Handel was half German, half Italian and half English. He was very large. Bach died from 1750 to the present. Beethoven wrote music even though he was deaf. He was so deaf he wrote loud music. He took long walks in the forest even when everyone was calling for him. Beethoven expired in 1827 and later died for this."
-t = t.lower()
+t = t.lower() #convert to lower
 t1=""
+#Remove everything that is not an alphabet
 for j in t:
     if j.isalpha():
         t1="".join([t1,j])
-print(t1)
-fd = nltk.FreqDist(t1)
+# print(t1)
+fd = nltk.FreqDist(t1) #get the freq distribution
 tot = len(t1) #length of text
-entropy = 0
+entropy,ent = 0,0
+ent_list = []
+probabs = {}
+print("Probabilities of each character in the text are: ")
 for i in fd:
     p = fd[i]/tot
-    print(i + "\t" + str(p)) #print probability
+    probabs[i]=p #create a dictionary with probabilities of alphabets
+    ent = -1*p*math.log2(p)
+    ent_list.append(ent)
     entropy += p*math.log2(p)
-print("entropy = " + str(-entropy))
+
+probabs = sorted(zip(probabs.values(), probabs.keys()), reverse=True)
+print(probabs) #prints probabilities of the alphabets in descending order
+
+print("\nentropy is = " + str(-entropy))
+# print(list(fd.values()))
+# print(ent_list)
+print("\nThe variances are: ")
+#Get the variance of the characters from the freq distribution
+print("variance of the characters:"+str(numpy.var(list(fd.values()))))
+#Get the variance of the entropies from the freq distribution
+print("variance of the entropies of characters:"+str(numpy.var(ent_list)))
+
     
     
--- A2/q3.py
+++ A2/q3.py
@@ -1,12 +1,12 @@
 '''
 Created on Sep 13, 2016
 
-@author: Bharat
+@author: Bharat Vemulapalli
 '''
 import sys,nltk,math
 
 def main(words,fname):
-    "Reads text from a file, prints token and frequency."     
+    #Reads text from a file    
     try:         
         file = open(fname, mode='r', encoding='utf-8')         
         text = file.readlines()         
@@ -14,25 +14,26 @@ def main(words,fname):
     except IOError:         
         print("Cannot read from file:", fname)         
         return
-    #print(text)
+    #pulls the POS tagger for in every line of the text and adds it to a list
     for x in text:
         words.append(x.split("\t")[2])
     
 def entropy(words):
+    #find the entropy of a list of words in a list
     entropy =0;
-    fd = nltk.FreqDist(words)
+    fd = nltk.FreqDist(words) #get frequency distribution
     tot = len(words)
     for f in fd:
         p = fd[f]/tot
         print(f+"\t"+str(p))
         entropy += p*math.log2(p)
     return entropy
-#     print( len(pos))
         
 
 if __name__ == '__main__':
-    pos = []
+    pos = [] #empty list for the list of POS taggers from all files
+    #Pass all the files in the arguments while running the program
+    #Reads all files i.e G01/G02.....G22
     for i in sys.argv[1:]:
         main(pos,i)
-    print("Entropy is :" + str(-entropy(pos)))
-#         print("file read: "+i)
+    print("\nEntropy is :" + str(-entropy(pos))) #prints the entropy
